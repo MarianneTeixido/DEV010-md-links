7620b2544f00da5a606eb66edea12b02bb708ec3
MarianneTeixido <ztp.txdo@gmail.com>
2023-10-02 21:30:34 -0600

    Create an obj with links information. Close #7

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const mdlinks = require('./lib/mdlinks'); // Ruta relativa al archivo mdlinks.js
 // Examples:
 
 //Correct
-mdlinks('./examples/readme.md');
+// mdlinks('./examples/readme.md');
 
-mdlinks('./examples/readme.js');
+//mdlinks('./examples/readme.js');
 //File not found 
-mdlinks('./examples/readme3.md');
+//mdlinks('./examples/readme3.md');
+
+mdlinks('./examples/readme.md')
+  .then((links) => {
+    console.log(links); // Imprime el objeto en la consola
+  })
+  .catch((error) => {
+    console.error(error);
+  });
