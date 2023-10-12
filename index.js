ccb6ccc03ca27a74c7d07fa08a3e7fd615fe4b04
MarianneTeixido <ztp.txdo@gmail.com>
2023-10-12 15:03:56 -0600

    read succesfully a path directory. Close #12

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const mdlinks = require('./lib/mdlinks'); // Ruta relativa al archivo mdlinks.js
 
 
-mdlinks('./examples/readme.md',true)
-  .then((links) => {
-    console.log(links); // Imprime el objeto en la consola
+// mdlinks('./examples/readme.md',true)
+//   .then((links) => {
+//     console.log(links); // Imprime el objeto en la consola
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+
+// mdlinks('./examples/readme.md')
+// .then((links) => {
+//   console.log(links); 
+// })
+// .catch((error) => {
+//   console.error(error);
+// });
+
+
+mdlinks("/home/lxow/git/CENTRO/2024-1/cc2-1-24-1")
+  .then(links => {
+    console.log(links);
   })
-  .catch((error) => {
-    console.error(error);
-  });
+  .catch(console.error);
 
 
-mdlinks('./examples/readme.md')
-.then((links) => {
-  console.log(links); 
-})
-.catch((error) => {
-  console.error(error);
-});
+// mdlinks("/home/lxow/git/CENTRO/2024-1/cc2-1-24-1", true)
+//   .then(links => {
+//     console.log(links);
+//   })
+//   .catch(console.error);
+
+
+// mdlinks("/home/lxow/git/CENTRO/2024-1/cc2-1-24-1", false)
+//   .then(links => {
+//     console.log(links);
+//   })
+//   .catch(console.error);
+
